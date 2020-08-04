21d8880f16fb1690b632e166baa416a3942a6833
Iurii Didkovskyi <hichnik@gmail.com>
2020-08-04 13:43:31 +0300

    Fixed path aliases

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PagenotfoundComponent } from './pagenotfound.component';
+import { PagenotfoundComponent } from '@app/pagenotfound.component';
+import { AuthComponent } from '@auth/auth.component';
+
 
 const routes: Routes = [
   {path: '', redirectTo: '/app', pathMatch: 'full'},
