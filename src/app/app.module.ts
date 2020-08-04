a56771b5e000b017ad190d0796c150268c336d3d
Iurii Didkovskyi <hichnik@gmail.com>
2020-08-04 13:23:08 +0300

    Changed project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagenotfoundComponent } from './pagenotfound.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AuthComponent } from './shared/auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    AboutusComponent,
+    ContactusComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
