5b5f19414089cced52e29d462ddf0a62094d72b7
Iurii Didkovskyi <hichnik@gmail.com>
2020-08-05 11:52:28 +0300

    Added toUppercase directive

--- src/app/shared/directives/uppercase.directive.ts
+++ src/app/shared/directives/uppercase.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
-  selector: '[appUppercase]'
+  selector: '[upperCase]'
 })
 export class UppercaseDirective {
 
-  constructor() { }
+  constructor(
+    private renderer2: Renderer2,
+    private elementRef: ElementRef<HTMLInputElement>
+  ) { }
+
+  @HostListener('input') OnChange(): void {
+    const { value } = this.elementRef.nativeElement;
+    console.log(`Uppercase directive: ${value}`);
+    this.renderer2.setProperty(this.elementRef.nativeElement, 'value', value.toUpperCase());
+  }
 
 }
