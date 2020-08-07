659f28ee7c28a91bc8ce618fa94d7e9089c56483
Iurii Didkovskyi <hichnik@gmail.com>
2020-08-07 04:51:37 +0300

    Finished with directives

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {fromEvent} from 'rxjs';
 
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.scss']
 })
-export class ContactusComponent implements OnInit {
+export class ContactusComponent implements OnInit, AfterViewInit {
 
   id: number;
+  @ViewChild('input') input: ElementRef<HTMLInputElement>;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,6 +18,15 @@ export class ContactusComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.id = params.id;
     });
+  }
+
+  onChange(v: string): void {
+    console.log(`value: ${v}`);
+  }
+
+  ngAfterViewInit() {
+    fromEvent(this.input.nativeElement, 'input')
+      .subscribe((v: any) => console.log(`fromEvent: ${v.target.value}`));
   }
 
 }
